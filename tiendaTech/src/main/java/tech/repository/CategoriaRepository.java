9640a872bf44bcf37b97f42a53e835f6976f0f85
MrEduCR~ <145067441+MrEduCR@users.noreply.github.com>
2025-10-07 15:41:53 -0600

    SEMANA 4 COMPLETA(semana 3 ya estaba incluido en commit anterior))

---
+++ tiendaTech/src/main/java/tech/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package tech.repository;
+ 
+import tech.domain.Categoria;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
+    public List<Categoria> findByActivoTrue();
+ 
+}

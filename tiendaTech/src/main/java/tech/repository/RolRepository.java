390077166f453e39b7a1427cff9068d8aab501e4
MrEduCR~ <145067441+MrEduCR@users.noreply.github.com>
2025-11-21 21:50:44 -0600

    SEMANA 11 Y 10 COMPLETAS

---
+++ tiendaTech/src/main/java/tech/repository/RolRepository.java
@@ -0,0 +1,11 @@
+package tech.repository;
+
+import tech.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+
+}
---
+++ tiendaTech/src/main/java/tech/repository/RutaRepository.java
@@ -0,0 +1,9 @@
+package tech.repository;
+
+import tech.domain.Ruta;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RutaRepository extends JpaRepository<Ruta, Integer> {
+    List<Ruta> findAllByOrderByRequiereRolAsc();
+}
